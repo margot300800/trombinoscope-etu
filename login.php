f68b08438db6339fd43f28a99ad66700f80a9981
Margot <margot300800@gmail.com>
2020-04-27 15:24:44 +0200

    mise en fonction ecritureCSV et recupDonnees + fonction vide hashmdp

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-$nomFamille = $_POST['nomFam'];
+/*$nomFamille = $_POST['nomFam'];
 $prenom = $_POST['prenom'];
 $aMail = $_POST['addMail'];
 $num = $_POST['numero'];
@@ -10,10 +10,36 @@ $groupeTD = $_POST['gpTD'];
 $photoP = $_POST['pp'];
 $mdp = $_POST['pwd'];
 $mdp2 = $_PODT['pwd2'];
-$baseCSVdonnee = fopen('inscrits.csv', 'a+');
+$baseCSVdonnee = fopen('inscrits_etu.csv', 'a+');*/
+
+
+function recupDonnees(){
+	$nomFamille = $_POST['nomFam'];
+	$prenom = $_POST['prenom'];
+	$aMail = $_POST['addMail'];
+	$num = $_POST['numero'];
+	$aPostale = $_POST['addPostale'];
+	$f = $_POST['filiere'];
+	$groupeTD = $_POST['gpTD'];
+	$photoP = $_POST['pp'];
+	$mdp = $_POST['pwd'];
+	$mdp2 = $_PODT['pwd2'];
+	return $nomFammile, $prenom, $aMail, $num, $aPostale, $f, $groupeTD, $photoP, $mdp, $mdp2;
+	}
+
+
+function hashmdp(mdp){
+	
+	
+	
+	}
+
+
+function ecritureCSV(){
+	$nomFammile, $prenom, $aMail, $num, $aPostale, $f, $groupeTD, $photoP, $mdp, $mdp2 = recupDonnees();
 
 // on vérifie que les champs sont bien remplis
-if(isset($_POST['pseudo'])){
+if(isset($_POST['nomFamille'])){
 	echo("Vous n'avez pas remplis tous les champs");
 	
 	}
@@ -21,7 +47,8 @@ else{
 	
 	//on vérifie que les 2 mots de passes sont identiques
 	if ($mdp=$mdp2){
-		$baseCSVdonnee = fopen('inscrits.csv', 'a+');
+		hashmdp("mdp");
+		$baseCSVdonnee = fopen('inscrits_etu.csv', 'a+');
 		
 		fwrite($baseCSVdonnee, $f);
 		fwrite($baseCSVdonnee, ";", 1);
@@ -53,4 +80,8 @@ else{
 		}
 	
 	}
+}	
+	
+ecritureCSV();
+	
 ?> 
