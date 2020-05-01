0e01f59628e66080d3ab64691f006b1f0faac0b7
Margot <margot300800@gmail.com>
2020-05-01 19:32:11 +0200

    réglages du beugs *8 (-> il fallait supprimer les accollades du foreach) et suppression du code inutile

--- login.php
+++ login.php
@@ -1,31 +1,6 @@
 <?php
 session_start();
-$nomFamille = $_POST['nomFam'];
-$prenom = $_POST['prenom'];
-$aMail = $_POST['addMail'];
-$num = $_POST['numero'];
-$aPostale = $_POST['addPostale'];
-$fili = $_POST['filiere'];
-$groupeTD = $_POST['gpTD'];
-$photoP = $_POST['pp'];
-$mdp = $_POST['pwd'];
-$mdp2 = $_POST['pwd2'];
-$baseCSVdonnee = fopen('inscrits_etu.csv', 'a+');
 
-
-/*function recupDonnees(){
-	$nomFamille = $_POST['nomFam'];
-	$prenom = $_POST['prenom'];
-	$aMail = $_POST['addMail'];
-	$num = $_POST['numero'];
-	$aPostale = $_POST['addPostale'];
-	$f = $_POST['filiere'];
-	$groupeTD = $_POST['gpTD'];
-	$photoP = $_POST['pp'];
-	$mdp = $_POST['pwd'];
-	$mdp2 = $_PODT['pwd2'];
-	return $nomFammile, $prenom, $aMail, $num, $aPostale, $f, $groupeTD, $photoP, $mdp, $mdp2;
-	}*/
 
 
 function hashmdp($mdp){
@@ -57,53 +32,16 @@ function ecritureCSV(){
 	$photoP = $_POST['pp'];
 	$mdp = $_POST['pwd'];
 	$mdp2 = $_POST['pwd2'];
-	$donneesEtu = array();
-	
-//	$baseCSVdonnee = fopen('inscrits_etu.csv', 'a+');
-
-	//$nomFammile, $prenom, $aMail, $num, $aPostale, $f, $groupeTD, $photoP, $mdp, $mdp2 = recupDonnees();
-
-/*// on vérifie que les champs sont bien remplis
-if(isset($_POST['nomFamille'])){
-	echo("Vous n'avez pas remplis tous les champs");
-	
-	}
-else{*/
 	
 	//on vérifie que les 2 mots de passes sont identiques
 	if ($mdp=$mdp2){
 		$mdp_hash = hashmdp($mdp);
-		$baseCSVdonnee = fopen('inscrits_etu.csv', 'a+');
 		$donneeEtu = array($fili, $groupeTD, $nomFamille, $prenom, $aMail, $num, $aPostale, $mdp_hash);
-		foreach ($donneeEtu as $line){
-			fputcsv($baseCSVdonnee,$donneeEtu);
-		}
-		/*fwrite($baseCSVdonnee, $fili);
-		fwrite($baseCSVdonnee, ";", 1);
-		
-		fwrite($baseCSVdonnee, $groupeTD);
-		fwrite($baseCSVdonnee, ";", 1);
-		
-		fwrite($baseCSVdonnee, $nomFamille);
-		fwrite($baseCSVdonnee, ";", 1);
-		
-		fwrite($baseCSVdonnee, $prenom);
-		fwrite($baseCSVdonnee, ";", 1);
-		
-		fwrite($baseCSVdonnee, $aMail);
-		fwrite($baseCSVdonnee, ";", 1);
-		
-		fwrite($baseCSVdonnee, $num);
-		fwrite($baseCSVdonnee, ";", 1);
-		
-		fwrite($baseCSVdonnee, $aPostale);
-		
-		fwrite($baseCSVdonnee, ";", 1);
-		fwrite($baseCSVdonnee, $mdp_hash);
-		fwrite($baseCSVdonnee, ";", 1);
-		fwrite($baseCSVdonnee, "\n", 2);
-		
-		fclose($baseCSVdonnee);*/
+		foreach ($donneeEtu as $line)
+			$baseCSVdonnee = fopen('inscrits_etu.csv', 'a+');
+			fputcsv($baseCSVdonnee,$donneeEtu, ";");
+			fclose($baseCSVdonnee);
+			
 		}
 	else{
 		echo("Les 2 mots de passe que vous avez rentrez ne sont pas identique");
